d95136a2e31f9610a951d7e4e8b2ed15318b04f7
박세준 <tpwns6693@gmail.com>
2022-06-24 06:06:18 +0900

    테이블을 헤치지 않는 선에서 UI 적용

--- src/components/PostPreviewComponent.jsx
+++ src/components/PostPreviewComponent.jsx
@@ -1,29 +1,70 @@
 import React from "react";
-import CommentIcon from '@material-ui/icons/Comment';
+import CommentIcon from "@material-ui/icons/Comment";
 import { Box } from "@material-ui/core";
 import { Link } from "react-router-dom";
-function PostPreviewComponent({title, detail, id, comment, symtomArray ,author, commentNumber,date}) {
-  const symtomTags = symtomArray.map((item, index) =>
-    index <= 4 ? <span className="symtom-tag">#{item.name}&nbsp;&nbsp;&nbsp;&nbsp;</span> : <></>
-  );
+function PostPreviewComponent({
+  title,
+  detail,
+  id,
+  symtomArray,
+  author,
+  date,
+  predict,
+  result,
+  canEdit,
+  onEditClick,
+  ...props
+}) {
+  const symtomTags = symtomArray.map((symptom) => (
+    <span key={symptom.symptomid} className="symtom-tag">
+      #{symptom.symptomname}&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+  ));
   return (
-    <Box className="border-bottom" component={Link} to={`community/:${id}`} style={{ textDecoration: 'none' ,color:"#000", display:"flex",flexDirection:"column",padding: "2rem 0.5rem"}}>
-      <div style={{ display: "flex" ,justifyContent:"space-between", marginBottom: "10px"}}>
+    <Box
+      className="border-bottom"
+      style={{
+        textDecoration: "none",
+        color: "#000",
+        display: "flex",
+        flexDirection: "column",
+        padding: "2rem 0.5rem",
+      }}
+      {...props}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "10px",
+        }}
+      >
         <span className="title text-bold">
           <h5 className="text-bold">{title}</h5>
         </span>
-        <span style={{color: "#588D23"}}>{symtomTags}</span>
+        <span style={{ color: "#588D23" }}>{symtomTags}</span>
       </div>
-        <p className='detail' style={{textAlign:"start"}}>{detail}</p>
-      <div className="post-footer" style={{color:"#A8A8A8", display: "flex" ,justifyContent:"space-between"}}>
+      <p className="detail" style={{ textAlign: "start" }}>
+        {detail}
+      </p>
+      <p>
+        
+        예측되는 질병 : {predict[0]},{predict[1]},{predict[2]}
+      </p>
+      <p> 진단 결과 : {result}</p>
+      <div
+        className="post-footer"
+        style={{
+          color: "#A8A8A8",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <div>
           <span>작성자&nbsp;| &nbsp;&nbsp;{author} </span>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{date}</span>
         </div>
-        <div>
-          <CommentIcon/>&nbsp;
-          {commentNumber}
-        </div>
+        {canEdit && <div onClick={onEditClick}>수정하기</div>}
       </div>
     </Box>
   );
