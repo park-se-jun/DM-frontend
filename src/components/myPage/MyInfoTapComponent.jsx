15dec860a4a19a15eee644a5d8387a687d241ad4
yu-podong <yu01gun@naver.com>
2022-06-24 03:21:42 +0900

    Delete: MyPostComponent, MyCommentComponent

--- src/components/myPage/MyInfoTapComponent.jsx
+++ src/components/myPage/MyInfoTapComponent.jsx
@@ -4,8 +4,8 @@ function MyInfoTapComponent() {
 	return (
 		<ul className='info-btn-wrap flex-horiz' style={{width: "100%", justifyContent: "center"}}>
 			<li className='info-item click'>정보</li>
-			<li className='info-item'>게시글</li>
-			<li className='info-item'>댓글</li>
+			{/* <li className='info-item'>게시글</li>
+			<li className='info-item'>댓글</li> */}
 		</ul>
 	);
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import MainLayout from "../components/MainLayout";
 
@@ -21,14 +21,15 @@ const Profile = () => {
   return (
     <MainLayout imagePath={MyPageImage} title={"마이페이지"} detail={"본인의 정보, 작성한 게시글 및 댓글 등을 볼 수 있습니다."}>
       <div className="container" style={{marginTop: "100px", marginBottom: "100px", }}>
+
         <MyInfoTap />
-        {/* <MyInfoComponent 
+        <MyInfoComponent 
           userName="userName"
           userEmail="userEmail@0000.com"
           userID="userID"
-        /> */}
+        />
         {/* <MyPostList /> */}
-        <MyComList />
+        {/* <MyComList /> */}
       </div>
     </MainLayout>
   );
