7f2d7fedfe2616a8920edc571969c52e8c0b43a1
SonMinSeock <zonins3@gmail.com>
2022-06-23 19:53:52 +0900

    Parsing 작업, 더보기 버튼 추가

--- src/components/disease/FilterContainer.jsx
+++ src/components/disease/FilterContainer.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import SymtomFillterComponent from "./SymtomFillterComponent";
+import disease from '../../db/disease.json';
+import symtoms from '../../db/symptoms.json';
+import ShowMore from 'react-show-more-button';
 
 function FilterContainer() {
+
+  let symtomsName = [];
+
+  const parsingData = () => {
+    symtoms.map(key =>symtomsName.push(key.symptomname));;
+  }
+
   return (
     <div className="border-bottom pb-4">
-      <div className="button_container ">
-        <SymtomFillterComponent key={"고열"} symtomName={"고열"}/>
-        <SymtomFillterComponent key={"복통"} symtomName={"복통"}/>
-        <SymtomFillterComponent key={"인후통"} symtomName={"인후통"}/>
-      </div>
+      {parsingData()}
+      <ShowMore maxHeight={300} styleButton={{backgroundColor: "#C9E5BB", border: '1px solid #C9E5BB', color: '#000000'}}>
+        <div className="button_container ">
+          {
+            symtoms.map((element, index) => index < symtomsName.length ? <SymtomFillterComponent key={"고열"} symtomName={symtomsName[index]}/> : null)
+          }
+        </div>
+      </ShowMore>
       <div className="search_container">
         <button className="search_button">검색</button>
       </div>
--- src/components/disease/SymtomFillterComponent.jsx
+++ src/components/disease/SymtomFillterComponent.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+import disease from '../../db/disease.json';
+import symtoms from '../../db/symptoms.json';
+
 
 function SymtomFillterComponent({symtomName}) {
+  
   const [value, setValue] = useState(0);
-    const colorSet = ["#FFFFFF","#EBFFC0","#DAFA97","#B8DF66","#96BF3F","#79A321"]
-    
+  //const [symtomsName, setSymtomsName] = useState([]);
+  let symtomsName = [];
+
+  const colorSet = ["#FFFFFF","#EBFFC0","#DAFA97","#B8DF66","#96BF3F","#79A321"];
+
+  const parsingData = () => {
+    symtoms.map(key =>symtomsName.push(key.symptomname));;
+  }
+
+
   return (
     <>
     <button className='disease_button' style={{backgroundColor:colorSet[value]}} onClick={()=>{setValue((prev)=>(prev+1)%6)}}>{symtomName}</button>
     </>
-
     
   )
 }
