c62f3a52b47a2f9fad4274f4924d51f7813555d9
박세준 <tpwns6693@gmail.com>
2022-06-23 14:07:03 +0900

    fix: 포스트 미리보는 컴포넌트 살짝 수정

--- src/components/PostPreviewComponent.jsx
+++ src/components/PostPreviewComponent.jsx
@@ -5,16 +5,14 @@ function PostPreviewComponent({title, detail, comment, symtomArray ,author, comm
     index <= 4 ? <span className="symtom-tag">#{item.name}</span> : <></>
   );
   return (
-    <div className="btn mt-5 border-bottom p-2" style={{display:"flex",flexDirection:"column" }}>
+    <div className="mt-5 border-bottom pb-4" role="button">
       <div style={{ display: "flex" ,justifyContent:"space-between"}}>
         <span className="title">
           <h5>{title}</h5>
         </span>
         <span style={{color: "#588D23"}}>{symtomTags}</span>
       </div>
-      <div className="post-detail">
-        <p>{detail}</p>
-      </div>
+        <p style={{textAlign:"start"}}>{detail}</p>
       <div className="post-footer" style={{color:"#A8A8A8", display: "flex" ,justifyContent:"space-between"}}>
         <span>작성자{"  "}| {author} </span>
         <span>게시일: {date} <CommentIcon/>{"  "} {commentNumber}</span>
